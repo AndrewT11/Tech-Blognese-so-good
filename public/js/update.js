0acb50fdc36a8844c0215a2e549e0b6ae76d1e2f
Andrew Tran <tranandrew@gmail.com>
2021-12-01 22:48:30 -0600

    modified js/update.js and post routes for update. add textarea for textValue input of update

--- public/js/update.js
+++ public/js/update.js
@@ -1,19 +1,19 @@
 const updateFormHandler = async (event) => {
   event.preventDefault();
 
-  const update = document.querySelector('#comment-desc').value.trim();
+  const update = document.querySelector('#update').value.trim();
   const postID = window.location.href[window.location.href.length - 1];
 
-  if (content) {
+  if (update) {
     const response = await fetch('/api/posts/' + postID, {
       method: 'POST',
-      body: JSON.stringify({ content }),
+      body: JSON.stringify({ update }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
     if (response.ok) {
-      document.location.replace('/post/' + postID);
+      document.location.replace('/posts/' + postID);
       //   document.location.reload()
     } else {
       alert('Failed to create comment');
